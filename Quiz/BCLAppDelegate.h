3bca17fbf83a8dc3a4f0a2bd558c4510222cec9e
Sujin Park <SujinPark@Sujins-MacBook-Pro.local>
2014-04-26 22:19:41 -0500

    Initial Commit

---
+++ Quiz/BCLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BCLAppDelegate.h
+//  Quiz
+//
+//  Created by Sujin Park on 4/26/14.
+//  Copyright (c) 2014 Boredcatlady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Sujin Park on 4/26/14.
+//  Copyright (c) 2014 Boredcatlady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BCLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BCLAppDelegate class]));
+    }
+}
